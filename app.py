ac467e1491a205dfca5daa12294ae4ece6ede66d
Rachel Turrell <rachelturrell@gmail.com>
2024-02-29 16:34:26 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get_home():
+    return render_template("home.html")
+
+@app.route('/page-<id>')
+def get_page(id):
+    return render_template(f"page-{id}.html")
+
+@app.route('/cats')
+def get_cats():
+    return render_template(f"cats.html")
+
+@app.route('/dogs')
+def get_dogs():
+    return render_template(f"dogs.html")
+
+if __name__ == '__main__':
+    app.run(
+      debug=True,
+      host="0.0.0.0" # Listen for connections _to_ any server
+    )
